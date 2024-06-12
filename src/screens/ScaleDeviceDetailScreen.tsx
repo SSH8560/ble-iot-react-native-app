38160b48e6ef26b46fc74389c1f6647ba09b4996
ssh8560 <ssh8560@naver.com>
2024-06-12 22:00:54 +0900

    feat: graph

--- src/screens/ScaleDeviceDetailScreen.tsx
+++ src/screens/ScaleDeviceDetailScreen.tsx
@@ -12,9 +12,17 @@ import {
 } from '@/apis/supabase/scaleDeviceValues';
 import {RootStackParams} from '@/router.d';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {Canvas, Circle, Line, Path, Skia} from '@shopify/react-native-skia';
+import {
+  Canvas,
+  Circle,
+  Line,
+  Path,
+  Skia,
+  Text,
+  useFont,
+} from '@shopify/react-native-skia';
 import React, {useEffect, useState} from 'react';
-import {View} from 'react-native';
+import {View, useWindowDimensions} from 'react-native';
 import dayjs from 'dayjs';
 
 interface ScaleDeviceDetailScreen
@@ -25,17 +33,16 @@ const ScaleDeviceDetailScreen = ({
     params: {device_id},
   },
 }: ScaleDeviceDetailScreen) => {
+  const {width} = useWindowDimensions();
   const [scaleValues, setScaleValues] = useState<SimpleScaleDeviceValue[]>([]);
+  const font = useFont(require('@/assets/fonts/NanumSquareR.ttf'), 18);
   useEffect(() => {
     getScaleDeviceValues(device_id).then(setScaleValues);
   }, [device_id]);
 
-  // scaleValues.forEach(v => console.log(dayjs(v.created_at).toDate()));
-
   const startOfDay = dayjs().startOf('day').toDate();
-  console.log(dayjs().startOf('day').toDate());
-  const endOfDay = dayjs().endOf('day').toDate();
-  const x = scaleUtc().domain([startOfDay, endOfDay]).range([0, 400]);
+  const endOfDay = dayjs().add(1, 'day').startOf('day').toDate();
+  const x = scaleUtc().domain([startOfDay, endOfDay]).range([0, width]);
   const y = scaleLinear().domain([-100, 1000]).range([400, 0]);
 
   const curvedLine = line<SimpleScaleDeviceValue>()
@@ -46,10 +53,29 @@ const ScaleDeviceDetailScreen = ({
   const linePath = Skia.Path.MakeFromSVGString(curvedLine);
   if (!linePath) return;
 
+  const xAxisData = Array.from({length: 11}, (_, i) => i).map(i => {
+    const evenHour = (i + 1) * 2;
+
+    return {
+      value: dayjs(startOfDay).hour(evenHour).toDate(),
+      label: `${evenHour}`,
+    };
+  });
+
   return (
-    <View style={{flex: 1}}>
-      <Canvas style={{width: 400, height: 400}}>
+    <View style={{flex: 1, alignItems: 'center'}}>
+      <Canvas style={{width, height: 400}}>
         <Path path={linePath} color={'#000'} style={'stroke'} strokeWidth={1} />
+        {xAxisData.map(({label, value}) => (
+          <Text
+            key={label}
+            x={x(value) - font!.measureText(label).width / 2}
+            y={400 * 0.99}
+            font={font}
+            text={label}
+            color="#000"
+          />
+        ))}
       </Canvas>
     </View>
   );
