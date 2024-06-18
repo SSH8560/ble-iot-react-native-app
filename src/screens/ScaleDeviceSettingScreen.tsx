ecf8af4fef76b77acf765333a37c77ddb3be864d
ssh8560 <ssh8560@naver.com>
2024-06-18 19:27:41 +0900

    feat: add header

--- src/screens/ScaleDeviceSettingScreen.tsx
+++ src/screens/ScaleDeviceSettingScreen.tsx
@@ -1,10 +1,13 @@
+import Header from '@/components/Header';
 import useBLE from '@/hooks/useBLE';
 import useScaleBLE from '@/hooks/useScaleBLE';
 import {RootStackParams} from '@/router.d';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import LottieView from 'lottie-react-native';
 import React, {useEffect, useState} from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, TouchableOpacity, View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {disconnect} from 'react-native-wifi-reborn';
 
 interface ScaleDeviceSettingScreenProps
   extends NativeStackScreenProps<RootStackParams, 'ScaleDeviceSetting'> {}
@@ -15,10 +18,36 @@ const ScaleDeviceSettingScreen = ({
     params: {device},
   },
 }: ScaleDeviceSettingScreenProps) => {
-  const {isConnected, scaleValue} = useScaleBLE(device.peripheral_id);
+  const {isConnected, scaleValue, connectDevice, disconnectDevice} =
+    useScaleBLE(device.peripheral_id);
 
   return (
     <View style={{flex: 1, gap: 16}}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          padding: 10,
+        }}>
+        <View style={{flexDirection: 'row', gap: 16, alignItems: 'center'}}>
+          <TouchableOpacity>
+            <Icon name="chevron-left" size={28} />
+          </TouchableOpacity>
+          <Text style={{fontSize: 20}}>
+            {device.device_name ?? device.peripheral_id}
+          </Text>
+        </View>
+        {isConnected ? (
+          <TouchableOpacity onPress={() => disconnectDevice()}>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>연결해제</Text>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity onPress={() => connectDevice()}>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>연결하기</Text>
+          </TouchableOpacity>
+        )}
+      </View>
       {isConnected ? (
         <View>
           <Text style={{color: '#000'}}>{scaleValue}</Text>
